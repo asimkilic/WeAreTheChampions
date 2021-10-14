2e0e3efb2d5f44ebf9171e87d347877fa9ac590c
asimkilic <a.asim.kilic@gmail.com>
2021-10-14 20:06:24 +0300

    TestConsole created

---
+++ WeAreTheChampions/TestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+           
+        }
+
+       
+    }
+}
