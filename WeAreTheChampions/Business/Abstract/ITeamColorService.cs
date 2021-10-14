35e5eca5c9b94d3fca15fc115ceaf4de0f7d05a1
asimkilic <a.asim.kilic@gmail.com>
2021-10-14 22:53:35 +0300

    TeamColor added in Business Layer

---
+++ WeAreTheChampions/Business/Abstract/ITeamColorService.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ITeamColorService
+    {
+        TeamColor GetByTeamId(int teamId);
+        TeamColor GetByColorId(int colorId);
+        List<TeamColor> GetAll();
+        void Add(TeamColor teamColor);
+        void Update(TeamColor teamColor);
+        void Delete(TeamColor teamColor);
+        
+    }
+}
---
+++ WeAreTheChampions/Business/Concrete/TeamColorManager.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class TeamColorManager : ITeamColorService
+    {
+        private ITeamColorDal _teamColorDal;
+
+        public TeamColorManager(ITeamColorDal teamColorDal)
+        {
+            _teamColorDal = teamColorDal;
+        }
+
+        public void Add(TeamColor teamColor)
+        {
+            _teamColorDal.Add(teamColor);
+        }
+
+        public void Delete(TeamColor teamColor)
+        {
+            _teamColorDal.Delete(teamColor);
+        }
+
+        public List<TeamColor> GetAll()
+        {
+            return _teamColorDal.GetAll();
+        }
+
+        public TeamColor GetByColorId(int colorId)
+        {
+            return _teamColorDal.Get(x => x.ColorId == colorId);
+        }
+
+        public TeamColor GetByTeamId(int teamId)
+        {
+            return _teamColorDal.Get(x => x.TeamId == teamId);
+        }
+
+        public void Update(TeamColor teamColor)
+        {
+            _teamColorDal.Update(teamColor);
+        }
+    }
+}
