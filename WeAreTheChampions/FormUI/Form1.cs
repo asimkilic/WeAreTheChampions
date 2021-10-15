2e343acd3c0ac02a755f27f67be8258a58217bfd
asimkilic <a.asim.kilic@gmail.com>
2021-10-15 20:44:05 +0300

    Inversion of Control adopted using the Dependency Inversion Principle

--- WeAreTheChampions/FormUI/Form1.cs
+++ WeAreTheChampions/FormUI/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business.Abstract;
+using Business.DependencyResolvers.Autofac;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,21 @@ namespace FormUI
 {
     public partial class Form1 : Form
     {
+        private IColorService _colorService;
         public Form1()
         {
             InitializeComponent();
+            _colorService = InstanceFactory.GetInstance<IColorService>();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var result = _colorService.GetAll();
+            foreach (var item in result)
+            {
+                MessageBox.Show(item.ColorName);
+            }
         }
     }
 }
