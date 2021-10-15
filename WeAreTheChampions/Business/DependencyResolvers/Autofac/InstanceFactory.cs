bbbac28f9c5eafadfb7fae0e1bc2b18f0da0728a
asimkilic <a.asim.kilic@gmail.com>
2021-10-15 20:29:40 +0300

    Instance Factory added

---
+++ WeAreTheChampions/Business/DependencyResolvers/Autofac/InstanceFactory.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class InstanceFactory
+    {
+        public static T GetInstance<T>()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new AutofacBusinessModule());
+            IContainer Container = builder.Build();
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                return scope.Resolve<T>();
+            }
+        }
+    }
+}
