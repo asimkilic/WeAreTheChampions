7e5c947b9df07184dcc02b41504fbc4bd460c048
asimkilic <a.asim.kilic@gmail.com>
2021-10-15 21:43:22 +0300

    Color Validation rules added

--- WeAreTheChampions/Business/Concrete/ColorManager.cs
+++ WeAreTheChampions/Business/Concrete/ColorManager.cs
@@ -1,6 +1,8 @@
 ﻿using Business.Abstract;
+using Business.ValidationRules.FluentValidation;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,16 +15,23 @@ namespace Business.Concrete
     {
         //TODO: ValidationAspect will add.
         //TODO: CacheAspect will add.
-      
+
         private IColorDal _colorDal;
         public ColorManager(IColorDal colorDal)
         {
-            
+
             _colorDal = colorDal;
         }
-        
+
         public void Add(Color color)
         {
+            var context = new ValidationContext<Color>(color);
+            ColorValidator colorValidator = new ColorValidator();
+            var result = colorValidator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
             //TODO: Check if exist.
             _colorDal.Add(color);
         }
