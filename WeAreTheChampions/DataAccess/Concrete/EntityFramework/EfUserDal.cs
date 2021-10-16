65663dbd7a0296fba412006588a8073a39fff8cb
asimkilic <a.asim.kilic@gmail.com>
2021-10-16 06:13:06 +0300

    UserService and AuthService added

---
+++ WeAreTheChampions/DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,28 @@
+﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : EfEntityRepositoryBase<User, ChampionsContext>, IUserDal
+    {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context = new ChampionsContext())
+            {
+                return context.Users
+                    .Where(x => x.Id == user.Id)
+                    .FirstOrDefault()
+                    .UserOperationClaims
+                    .Select(x => x.OperationClaim)
+                    .ToList();
+
+            }
+        }
+    }
+}
